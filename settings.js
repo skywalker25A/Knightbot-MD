0a3b1ebdeaff159c19179683da4633165aad031b
skywalker25A <yeboahmirekuelisha@gmail.com>
2025-01-30 04:27:30 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
-+233555743562const settings = {
+const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'skywaker', // Your name
+  ownerNumber: '+233555743562', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
